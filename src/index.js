d657136e2b0f7d71dc4f0a8325d8573e8be8b23b
radhikapatidar07 <radhikachawda777@gmail.com>
2023-11-28 11:18:33 +0530

    redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./service/reducers/index";
+const store = createStore(rootReducer);
+//console.warn("store data",store)
+//
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Provider />
   </React.StrictMode>
 );
 
---
+++ src/services/Reducers/reducer.js
@@ -0,0 +1,20 @@
+import { ADD_TO_CART, REMOVE_TO_CART } from "./constants";
+const initialState = {
+  cardData: [],
+};
+
+export default function cardItems(state = initialState, action) {
+  //eslint-disable-next-line default-case
+  switch (action.type) {
+    case ADD_TO_CART:
+      console.warn("reducer", action);
+      return [...state, (cardData = action.data)];
+    case REMOVE_TO_CART:
+      console.warn("reducer", action);
+      state.pop();
+      return [...state];
+      break;
+    default:
+      return state;
+  }
+}
